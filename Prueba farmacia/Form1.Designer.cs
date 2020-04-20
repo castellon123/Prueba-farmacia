96354629a132ea40871ed556e7aec058a69ec320
castellon123 <63824225+castellon123@users.noreply.github.com>
2020-04-20 12:58:11 -0600

    se agregaron cambios

--- Prueba farmacia/Form1.Designer.cs
+++ Prueba farmacia/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Prueba_farmacia
 {
-    partial class Form1
+    partial class fmrClientes
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,260 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_tel = new System.Windows.Forms.TextBox();
+            this.txt_correo = new System.Windows.Forms.TextBox();
+            this.txt_direccion = new System.Windows.Forms.TextBox();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.txt_buscar = new System.Windows.Forms.TextBox();
+            this.ConsultaDatos = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.GroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ConsultaDatos)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(58, 433);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(77, 20);
+            this.Label1.TabIndex = 19;
+            this.Label1.Text = "BUSCAR";
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.txt_tel);
+            this.GroupBox1.Controls.Add(this.txt_correo);
+            this.GroupBox1.Controls.Add(this.txt_direccion);
+            this.GroupBox1.Controls.Add(this.txt_id);
+            this.GroupBox1.Controls.Add(this.txt_nombre);
+            this.GroupBox1.Controls.Add(this.Label6);
+            this.GroupBox1.Controls.Add(this.Label5);
+            this.GroupBox1.Controls.Add(this.Label4);
+            this.GroupBox1.Controls.Add(this.Label3);
+            this.GroupBox1.Controls.Add(this.Label2);
+            this.GroupBox1.Location = new System.Drawing.Point(30, 77);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(471, 329);
+            this.GroupBox1.TabIndex = 18;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Datos del Cliente";
+            // 
+            // txt_tel
+            // 
+            this.txt_tel.Location = new System.Drawing.Point(122, 266);
+            this.txt_tel.Name = "txt_tel";
+            this.txt_tel.Size = new System.Drawing.Size(340, 26);
+            this.txt_tel.TabIndex = 20;
+            // 
+            // txt_correo
+            // 
+            this.txt_correo.Location = new System.Drawing.Point(123, 216);
+            this.txt_correo.Name = "txt_correo";
+            this.txt_correo.Size = new System.Drawing.Size(339, 26);
+            this.txt_correo.TabIndex = 19;
+            // 
+            // txt_direccion
+            // 
+            this.txt_direccion.Location = new System.Drawing.Point(123, 167);
+            this.txt_direccion.Name = "txt_direccion";
+            this.txt_direccion.Size = new System.Drawing.Size(339, 26);
+            this.txt_direccion.TabIndex = 18;
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(161, 112);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(301, 26);
+            this.txt_id.TabIndex = 17;
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(107, 49);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(355, 26);
+            this.txt_nombre.TabIndex = 16;
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Location = new System.Drawing.Point(6, 266);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(94, 20);
+            this.Label6.TabIndex = 15;
+            this.Label6.Text = "TELEFONO";
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(6, 216);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(79, 20);
+            this.Label5.TabIndex = 14;
+            this.Label5.Text = "CORREO";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(6, 167);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(99, 20);
+            this.Label4.TabIndex = 13;
+            this.Label4.Text = "DIRECCION";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(6, 112);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(138, 20);
+            this.Label3.TabIndex = 12;
+            this.Label3.Text = "IDENTIFICACION";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(6, 49);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(79, 20);
+            this.Label2.TabIndex = 11;
+            this.Label2.Text = "NOMBRE";
+            // 
+            // txt_buscar
+            // 
+            this.txt_buscar.Location = new System.Drawing.Point(153, 430);
+            this.txt_buscar.Name = "txt_buscar";
+            this.txt_buscar.Size = new System.Drawing.Size(338, 26);
+            this.txt_buscar.TabIndex = 17;
+            // 
+            // ConsultaDatos
+            // 
+            this.ConsultaDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ConsultaDatos.Location = new System.Drawing.Point(562, 90);
+            this.ConsultaDatos.Name = "ConsultaDatos";
+            this.ConsultaDatos.RowTemplate.Height = 28;
+            this.ConsultaDatos.Size = new System.Drawing.Size(382, 363);
+            this.ConsultaDatos.TabIndex = 20;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(999, 31);
+            this.toolStrip1.TabIndex = 21;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::Prueba_farmacia.Properties.Resources.nuevo;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(28, 28);
+            this.toolStripButton1.Text = "btn_agregar";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::Prueba_farmacia.Properties.Resources.editar;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(28, 28);
+            this.toolStripButton2.Text = "btn_editar";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = global::Prueba_farmacia.Properties.Resources.guardar;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(28, 28);
+            this.toolStripButton3.Text = "btn_guardar";
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = global::Prueba_farmacia.Properties.Resources.cancelar;
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(28, 28);
+            this.toolStripButton4.Text = "btn_cancelar";
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = global::Prueba_farmacia.Properties.Resources.eliminar;
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(28, 28);
+            this.toolStripButton5.Text = "btn_eliminar";
+            // 
+            // fmrClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(999, 526);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.ConsultaDatos);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.txt_buscar);
+            this.Name = "fmrClientes";
+            this.Text = "Clientes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ConsultaDatos)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.TextBox txt_tel;
+        internal System.Windows.Forms.TextBox txt_correo;
+        internal System.Windows.Forms.TextBox txt_direccion;
+        internal System.Windows.Forms.TextBox txt_id;
+        internal System.Windows.Forms.TextBox txt_nombre;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.TextBox txt_buscar;
+        internal System.Windows.Forms.DataGridView ConsultaDatos;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
     }
 }
 
--- Prueba farmacia/Form1.cs
+++ Prueba farmacia/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Prueba_farmacia
 {
-    public partial class Form1 : Form
+    public partial class fmrClientes : Form
     {
-        public Form1()
+        public fmrClientes()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
